e26fba4a0585b2e59eb65bf4594ef7a98c72bf27
richodia1 <okosodorichard@yahoo.com>
2020-04-10 06:06:00 +0100

    ImplementingRestDataRepositoryLibrary

--- RestDataRepository/RegisterRepo.cs
+++ RestDataRepository/RegisterRepo.cs
@@ -1,10 +1,90 @@
-﻿using System;
+﻿using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace RestDataRepository
 {
    public class RegisterRepo
     {
+        private const string BaseUrl = "https://reqres.in";
+        private readonly RestSharp.IRestClient _client;
+        RestSharp.IRestResponse restResponse;
+        RestSharp.RestRequest request;
+        public RegisterRepo()
+        {
+            _client = new RestClient(BaseUrl);
+            request = new RestRequest("api/register", Method.POST);
+        }
+        public async Task<IRestResponse<RegisterResponse>> SuccessfulRegistration(string email, string password)
+        {
+            if (email != null && password != null)
+            {
+                RegisterRequest req = new RegisterRequest();
+                req.email = email;
+                req.password = password;
+                request.RequestFormat = DataFormat.Json;
+                request.AddJsonBody(req);
+            }
+            return await _client.ExecutePostAsync<RegisterResponse>(request);
+        }
+        public async Task<IRestResponse<ErrorResponse>> UnsuccessfulRegistration(string email, string password)
+        {
+
+            if (!string.IsNullOrEmpty(email) && string.IsNullOrEmpty(password))
+            {
+                BadRegisterRequest req = new BadRegisterRequest();
+                req.email = email;
+                request.RequestFormat = DataFormat.Json;
+                request.AddJsonBody(req);
+            }
+            return await _client.ExecutePostAsync<ErrorResponse>(request);
+        }
+        public int getRegisterationStatusCode(string email, string password)
+        {
+
+            if (email != null && password != null)
+            {
+                RegisterRequest req = new RegisterRequest();
+                req.email = email;
+                req.password = password;
+                request.RequestFormat = DataFormat.Json;
+                restResponse = _client.Execute(request);
+            }
+            else if (email != null && password == null)
+            {
+                BadRegisterRequest req = new BadRegisterRequest();
+                req.email = email;
+                request.RequestFormat = DataFormat.Json;
+                restResponse = _client.Execute(request);
+            }
+
+            //Extract status code from received response and store as an Interger
+            return (int)restResponse.StatusCode;
+
+        }
+
     }
 }
+public class RegisterResponse
+{
+    public int id { get; set; }
+    public string token { get; set; }
+}
+public class ErrorResponse
+{
+    public string error { get; set; }
+
+}
+public class BadRegisterRequest
+{
+    public string email { get; set; }
+
+}
+class RegisterRequest
+{
+    public string email { get; set; }
+    public string password { get; set; }
+}
+
--- RestDataRepository/UsersRepo.cs
+++ RestDataRepository/UsersRepo.cs
@@ -1,10 +1,58 @@
-﻿using System;
+﻿using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace RestDataRepository
 {
    public class UsersRepo
     {
+        private const string BaseUrl = "https://reqres.in";
+        private readonly IRestClient _client;
+        private IRestResponse restResponse;
+        private RestRequest request;
+        public UsersRepo()
+        {
+            _client = new RestClient(BaseUrl);
+            request = new RestRequest("api/users?page=2", Method.GET);
+        }
+
+        public async Task<IRestResponse<Users>> GetUsersList()
+        {
+            return await _client.ExecuteGetAsync<Users>(request);
+        }
+        public int getUsersStatusCode()
+        {
+            restResponse = _client.Execute(request);
+            //Extract status code from received response and store as an Interger
+            return (int)restResponse.StatusCode;
+
+        }
     }
+}
+public class Users
+{
+
+    public int page { get; set; }
+    public int per_page { get; set; }
+    public int total { get; set; }
+    public int total_pages { get; set; }
+    public Datum[] data { get; set; }
+    public Ad ad { get; set; }
+
+}
+public class Datum
+{
+    public int id { get; set; }
+    public string email { get; set; }
+    public string first_name { get; set; }
+    public string last_name { get; set; }
+    public string avatar { get; set; }
+}
+public class Ad
+{
+    public string company { get; set; }
+    public string url { get; set; }
+    public string text { get; set; }
 }
